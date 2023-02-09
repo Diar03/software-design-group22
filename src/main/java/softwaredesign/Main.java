36d8b4953a0721e0f6e30a1ae2ecf5983f5f29d6
Darian <darian.janevski@gmail.com>
2023-02-09 15:36:09 +0100

    TESTING

--- src/main/java/softwaredesign/Main.java
+++ src/main/java/softwaredesign/Main.java
@@ -3,5 +3,8 @@ package softwaredesign;
 public class Main {
     public static void main (String[] args){
         System.out.println("Welcome to Software Design");
+        System.out.println("test from ibo");
+
+        System.out.println("KOSTA IS RESPONSIBLE FOR EVERY ERROR");
     }
 }
