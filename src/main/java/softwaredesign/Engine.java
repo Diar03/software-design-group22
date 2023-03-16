d7d9e1f5209dea54027c278f007d39d6b2df1254
Darian <darian.janevski@gmail.com>
2023-03-16 17:48:31 +0100

    Adding Engine class and Initializer class.

---
+++ src/main/java/softwaredesign/Engine.java
@@ -0,0 +1,12 @@
+package softwaredesign;
+
+public class Engine {
+    Initalizer initalizer;
+    Creature creature;
+    Environment environment;
+    //add the map with String to Image
+
+    static void displayMainScreen() {}
+    static void displayGameMenu() {}
+    static void handleDeath() {}
+}
---
+++ src/main/java/softwaredesign/Initalizer.java
@@ -0,0 +1,10 @@
+package softwaredesign;
+
+public class Initalizer {
+    String path;
+    //Add load resources
+
+   // static Environment makeEnvironment(String environment) {return new Environment();}
+   // static Creature makeCreature(String creature) {return}
+
+}
