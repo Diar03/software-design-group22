f168689b5315dbe1f99651e58513c5032b370fe6
Kosta Bakalinov <kbakalinov13@gmail.com>
2023-03-22 19:37:47 +0100

    Mini Game abstract class

--- src/main/java/com/example/softwaredesign/MiniGame.java
+++ src/main/java/com/example/softwaredesign/MiniGame.java
@@ -1,5 +1,13 @@
 package com.example.softwaredesign;
 
 public abstract class MiniGame {
+    private String name;
+
+    public MiniGame(String miniGameName){
+        this.name = miniGameName;
+    }
+    //public abstract Boolean awardCoins();
+    //public abstract void endGame();
 
 }
+
